07f60079252456e63b17da86fb8ad2075cfb8aa2
ashutosh7034 <ashutosh3276s16@gmail.com>
2026-09-23 20:53:45 +0530

    chore: clean up project artifacts

--- scratch/desc_history.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . "/../database/db_connect.php";
-$db = new DBController();
-$conn = $db->conn;
-
-$res = mysqli_query($conn, "DESCRIBE st_student_semester_history");
-while ($r = mysqli_fetch_assoc($res)) {
-    echo "{$r['Field']} ({$r['Type']})\n";
-}
--- scratch/desc_nptel_records.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/../database/db_connect.php';
-$db = new DBController();
-$res = mysqli_query($db->conn, "DESC st_nptel_records");
-while ($r = mysqli_fetch_assoc($res)) {
-    print_r($r);
-}
